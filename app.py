65f63c04d9fa4fe0447859ad3dfd8872fc86dacd
Talha-Altair <cooltalha27@gmail.com>
2021-09-19 15:01:09 +0530

    Updates for 2021-09-19 15:01:09

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
 from flask import *
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+
+UPLOAD_PATH = 'static/'
+
+FILE_NAME = 'user-feedback.csv'
 
 @app.route('/')
 def start():
 
-    return render_template('index.html')
+    return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_files():
+
+    uploaded_file = request.files['file']
+
+    filename = secure_filename(uploaded_file.filename)
+
+    fileext = filename.split('.')[-1]
+
+    if filename != '' and fileext == 'csv':
+
+        uploaded_file.save(os.path.join(UPLOAD_PATH, filename))
+
+    else:
+
+        return "Please send CSV File", 500
+
+    return '', 204
+
+@app.route('/home')
+def home():
+
+    result = 0
+
+    return render_template('index.html', result = result)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
